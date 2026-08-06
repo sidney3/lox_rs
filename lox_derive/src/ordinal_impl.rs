ba9e4bfc060603909eaffb893d5cbe0b8508e55e
Sidney3 <sidneyarthurlevine1@hotmail.com>
2026-08-05 22:31:10 -0400

    ordinal_impl submodule

---
+++ lox_derive/src/ordinal_impl.rs
@@ -0,0 +1,43 @@
+use quote::quote;
+use syn::Data;
+
+pub fn ordinal_impl(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+  let input = syn::parse_macro_input!(input as syn::DeriveInput);
+
+  expand(input).unwrap_or_else(|e| e.to_compile_error().into())
+}
+
+fn expand(input: syn::DeriveInput) -> syn::Result<proc_macro::TokenStream> {
+  let name = &input.ident;
+
+  let variants = match &input.data {
+    Data::Enum(e) => &e.variants,
+    _ => {
+      return Err(syn::Error::new_spanned(
+        &input.ident,
+        "Ordinal can only be used on enums",
+      ));
+    }
+  };
+
+  let variant_count = variants.len();
+
+  let expanded = quote! {
+      // TODO: figure out how to refer to the
+      // actual crate
+      impl crate::core::Ordinal for #name {
+          const COUNT: usize = #variant_count;
+
+          fn ord(&self) -> usize {
+              *self as usize
+          }
+      }
+
+      impl Copy for #name {}
+      impl Clone for #name {
+          fn clone (&self) -> Self {*self}
+      }
+  };
+
+  Ok(expanded.into())
+}
